887bb553b301dcf882118425cca3f1e33cb4e89e
dmarenin <39666107+dmarenin@users.noreply.github.com>
2020-04-02 18:50:55 +0500

    update etc files

---
+++ MyBonusProg/bak_db.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cp -a /MyBonusProg/MyBonusProg/db.db /MyBonusProg/MyBonusProg/bak/$(date +%Y_%m_%d_%H_%M_%S)
--- MyBonusProg/start.sh
+++ MyBonusProg/start.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-screen -dmUS my_bonus_prog python3.5 runserver.py
+screen -dmUS my_bonus_prog python3.5 /MyBonusProg/MyBonusProg/runserver.py
